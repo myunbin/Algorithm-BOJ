caf8263c79b6b9dc1a9f6e62dac0bfac7597735d
Hyunbin Kim <akim9905@gmail.com>
2021-01-03 14:22:40 +0900

    회의실 배정 2
    priority queue, greedy
    
    희의실 배정 2, 3, 4는 코드가 똑같다.
    상위문제인 4를 풀었으니까..

---
+++ Code/19621.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tp;
+const int MAX_N = 1e5 + 1;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+  	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+	
+	vector<tp> v;
+	for (int i = 0; i < n; i++) {
+		int s, e, c;
+		cin >> s >> e >> c;
+		v.emplace_back(s, e, c);
+	}
+	sort(v.begin(), v.end());
+
+	priority_queue<pii, vector<pii>, greater<pii>> pq;
+	
+	int ep = 0;
+	int ans = -1;
+	for (int i = 0; i < n; i++) {
+		auto [curs, cure, curc] = v[i];
+		while (!pq.empty() && pq.top().first < curs) {
+			ep = max(ep, pq.top().second);
+			pq.pop();
+		}
+		pq.push({cure, ep + curc});
+		ans = max(ans, ep + curc);
+	}
+	
+	cout << ans << '\n';
+	
+	return 0;
+}
