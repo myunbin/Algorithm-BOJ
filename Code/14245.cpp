0ffde35e21810fc5856bbf71344e47c8644b5cb2
Hyunbin Kim <akim9905@gmail.com>
2021-01-06 23:41:55 +0900

    XOR
    segment tree, lazy propagation
    
    a^a = 0, a^0 = a, a^b^b = a^0 = a 라는 점을 이용해서 lazy 배열을 관리해주면 된다.
    만약에 segment tree가 담당하고 있는 노드의 개수 (구간의 길이)가 짝수라면, lazy값은 0일 것이다. 
    
    이를 이용해 lazy 배열을 작성해주면 된다.
    처음에 연산 관련 성질들을 제대로 깨우치지 못한채로 문제에 접근했다가 아예 손도 못댔다.

---
+++ Code/14245.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0)
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+using namespace std;
+typedef long long ll;
+vector<ll> tree, lazy, a;
+
+ll init(int node, int le, int ri) {
+	if (le == ri) return tree[node] = a[le];
+	int mid = le + ri >> 1;
+	return tree[node] = init(node * 2, le, mid) ^ init(node * 2 + 1, mid + 1, ri);
+}
+
+void propagate(int node, int le, int ri) {
+	if (lazy[node] != 0) {
+		tree[node] ^= lazy[node] * ((ri - le + 1) % 2);
+		if (le != ri) {
+			lazy[node * 2] ^= lazy[node];
+			lazy[node * 2 + 1] ^= lazy[node];
+		}
+		lazy[node] = 0;
+	}
+}
+
+void update(int node, int le, int ri, int ql, int qr, int d) {
+	propagate(node, le, ri);
+	if (ri < ql || qr < le) return;
+	if (ql <= le && ri <= qr) {
+		lazy[node] ^= d;
+		propagate(node, le, ri);
+		return;
+	}
+	int mid = le + ri >> 1;
+	update(node * 2, le, mid, ql, qr, d);
+	update(node * 2 + 1, mid + 1, ri, ql, qr, d);
+	tree[node] = tree[node * 2] ^ tree[node * 2 + 1];
+}
+
+ll query(int node, int le, int ri, int ql, int qr) {
+	propagate(node, le, ri);
+	if (ri < ql || qr < le) return 0;
+	if (ql <= le && ri <= qr) return tree[node];
+	int mid = le + ri >> 1;
+	return query(node * 2, le, mid, ql, qr) ^ query(node * 2 + 1, mid + 1, ri, ql, qr);
+}
+
+int main() {
+	fio();
+	int n, m;
+	cin >> n;
+	a.resize(n); tree.resize(4 * n); lazy.resize(4 * n);
+	for (int i = 0; i < n; i++) cin >> a[i];
+	init(1, 0, n - 1);
+	
+	cin >> m;
+	while (m--) {
+		int t, a, b, c;
+		cin >> t >> a;
+		if (t == 1) {
+			cin >> b >> c;
+			update(1, 0, n - 1, a, b, c);
+		}
+		else cout << query(1, 0, n - 1, a, a) << '\n';
+	}
+	
+	return 0;
+}
