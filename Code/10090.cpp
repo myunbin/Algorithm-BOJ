2217b1a8980ef35db4d194ed0828776bddb31aed
Hyunbin Kim <akim9905@gmail.com>
2021-01-10 16:16:30 +0900

    Counting Inversions
    merge sort
    
    inversion counting 이라는 굉장히 유명한 문제다.
    merge sort의 merge 과정에서 오른쪽 부분 배열을 항상 왼쪽 배열에 붙인다고 생각했을 때,
    오른쪽 배열에 속해 있는 한 원소가 왼쪽배열에 병합되면서 건너뛰어야하는 원소의 개수만큼씩 계속 더해주는 게 답이다.

---
+++ Code/10090.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0)
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+using namespace std;
+typedef long long ll;
+const int MAX_N = 1e6 + 1;
+int n, a[MAX_N], s[MAX_N];
+ll ans;
+
+void merge(int le, int ri) {
+	int mid = le + ri >> 1;
+	int lo = le, hi = mid + 1;
+	int k = le;
+	
+	while (lo <= mid || hi <= ri) {
+		if (hi > ri || (lo <= mid && a[lo] <= a[hi])) s[k++] = a[lo++];
+		else {
+			ans += (ll)(mid - lo + 1);
+			s[k++] = a[hi++];
+		}
+	}
+	for (int i = le; i <= ri; i++) a[i] = s[i];
+}
+
+void mergeSort(int le, int ri) {
+	if (le >= ri) return;
+	
+	int mid = le + ri >> 1;
+	mergeSort(le, mid); mergeSort(mid + 1, ri);
+	merge(le, ri);
+}
+
+int main() {
+	fio();
+	
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> a[i];
+	mergeSort(0, n - 1);
+	
+	cout << ans << '\n';
+	
+	return 0;
+}
