534a65d4f83ed393d381158d8dea9d9d645bf66b
Hyunbin Kim <akim9905@gmail.com>
2021-02-13 01:40:08 +0900

    Create default.cpp

---
+++ default.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0)
+#define fileio() ifstream file_in("input.txt");ofstream file_out("output.txt")
+#define f first
+#define s second
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tpi;
+typedef tuple<ll, ll, ll> tpl;
+typedef pair<double, ll> pdl;
+typedef pair<double, int> pdi;
+const int MOD = 1000000007; // PLZ chk
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = 2e5; // PLZ chk
+
+int main() {
+	fio();
+	return 0;
+}
