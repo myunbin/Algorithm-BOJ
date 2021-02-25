4793871ed225aeca1093495af55adc9a80e99a63
Hyunbin Kim <akim9905@gmail.com>
2021-02-25 19:33:53 +0900

    Create ccw.cpp

---
+++ Reference/ccw.cpp
@@ -0,0 +1,11 @@
+int ccw(Point p1, Point p2, Point p3){
+	ll t1 = p1.x * p2.y + p2.x * p3.y + p3.x * p1.y;
+	ll t2 = p1.y * p2.x + p2.y * p3.x + p3.y * p1.x;
+	
+	// t1-t2의 부호를 반환
+	if(t1==t2) return 0;
+	else return t1<t2 ? -1 : 1; 
+}
+
+// 반시계: -1, 시계: 1, 한직선: 0
+//https://www.acmicpc.net/blog/view/27
