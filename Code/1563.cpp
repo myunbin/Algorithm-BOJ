3ad6554262adf45b3d1ce81c1bbe071d5d7435a5
Hyunbin Kim <akim9905@gmail.com>
2021-03-01 23:32:17 +0900

    개근상
    dp
    
    dp[i][j][k] = "현재 i번째 날에서 지각이 j번, 결석이 연속 k번인 상태에서 시작했을 때 경우의 수"
    라고 정의하면 dp[i][j][k]는 오늘 출석하는 경우, 지각하는 경우, 결석하는 경우 세가지의 합이다.

---
+++ Code/1563.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+#define pb(x) push_back(x)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0)
+#define fileio() ifstream file_in("boards.txt");ofstream file_out("boards.txt")
+#define f first
+#define s second
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tpi;
+typedef tuple<ll, ll, ll> tpl;
+typedef pair<double, ll> pdl;
+typedef pair<double, int> pdi;
+const int MOD = 1e6;
+const ll LMOD = 1e9+7; // PLZ check
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = 1e3 + 10;
+const int MAXR = 2e5 + 10;
+int n, dp[MAXN][3][4];
+
+int go(int i, int j, int k) {
+	if (i == n) return 1;
+	int &ret = dp[i][j][k];
+	if (ret != -1) return ret;
+
+	ret = go(i+1, j, 0) % MOD;
+	if (j < 1) ret += go(i+1, j+1, 0) % MOD;
+	if (k < 2) ret += go(i+1, j, k+1) % MOD;
+	ret %= MOD;
+	return ret;
+}
+
+int main() {
+	fio();
+	memset(dp, -1, sizeof(dp));
+	cin >> n;
+	cout << go(0, 0, 0) << endl;
+	return 0;
+}
