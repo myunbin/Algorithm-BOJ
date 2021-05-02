7d8c1b024b22201f37828191f65ee105e16fd3bb
Hyunbin Kim <akim9905@gmail.com>
2021-05-02 19:21:12 +0900

    벌집
    수학

---
+++ Code/2292.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+#define pb(x) push_back(x)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0)
+#define fileio() ifstream file_in("cowjump.in");ofstream file_out("cowjump.out")
+#define f first
+#define s second
+#define double long double
+#define db double
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tpi;
+typedef tuple<ll, ll, ll> tpl;
+typedef pair<double, ll> pdl;
+typedef pair<double, int> pdi;
+typedef pair<double, double> pdd;
+const int MOD = 987654321;
+const ll LMOD = 1e9+7; // PLZ check
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = 101;
+const int MAXR = 2e5 + 10;
+
+int main() {
+	fio();
+	int n; 
+	cin >> n;
+	if (n == 1) {
+		cout << 1 << endl;
+		return 0;
+	}
+	for (ll i=2;; i++) {
+		ll r1 = 3*(i-2)*(i-2)+3*(i-2)+2;
+		ll r2 = 3*(i-1)*(i-1)+3*(i-1)+1;
+		if (r1 <= n && n <= r2) { 
+			cout << i << endl;
+			return 0;	
+		}
+	}	
+	return 0;
+}
