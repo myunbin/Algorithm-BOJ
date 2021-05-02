a0dc502b8569496a69b8726934e1fc17934d71bf
Hyunbin Kim <akim9905@gmail.com>
2021-05-02 19:23:20 +0900

    ACM 호텔
    수학

---
+++ Code/10250.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+#define pb(x) push_back(x)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0)
+#define fileio() ifstream file_in("cowjump.in");ofstream file_out("cowjump.out")
+#define f first
+#define s second
+#define double long double
+#define db double
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tpi;
+typedef tuple<ll, ll, ll> tpl;
+typedef pair<double, ll> pdl;
+typedef pair<double, int> pdi;
+typedef pair<double, double> pdd;
+const int MOD = 1234567891;
+const ll LMOD = 1e9+7; // PLZ check
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = 101;
+const int MAXR = 2e5 + 10;
+void solve() {
+	int h, w, n;
+	cin >> h >> w >> n;
+	int t1 = n%h, t2 = n/h+1;
+
+	if (!t1) t1 = h, t2--;
+	cout << t1*100+t2 << endl;
+}
+int main() {
+	fio();
+	int t;
+	cin >> t;
+	while (t--) solve();
+	
+	return 0;
+}
