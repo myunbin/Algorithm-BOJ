50589fda90f90fb83d6dcf1b85e9ccd017573eb2
Hyunbin Kim <akim9905@gmail.com>
2021-05-02 17:25:11 +0900

    아무래도이문제는A번난이도인것같다
    구현
    
    무조건 yes.

---
+++ Code/1402.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+#define pb(x) push_back(x)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0)
+#define fileio() ifstream file_in("cowjump.in");ofstream file_out("cowjump.out")
+#define f first
+#define s second
+#define double long double
+#define db double
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tpi;
+typedef tuple<ll, ll, ll> tpl;
+typedef pair<double, ll> pdl;
+typedef pair<double, int> pdi;
+typedef pair<double, double> pdd;
+const int MOD = 987654321;
+const ll LMOD = 1e9+7; // PLZ check
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = 101;
+const int MAXR = 2e5 + 10;
+
+void solve() {
+	int a, b; cin >> a >> b;
+	cout << "yes" << endl;
+}
+int main() {
+	fio();
+	int t; cin >> t;
+	while (t--) solve();
+	return 0;
+}
