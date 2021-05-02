7090f9b2ba76e45726baf875ad0b5f31ad421200
Hyunbin Kim <akim9905@gmail.com>
2021-05-02 16:31:24 +0900

    세 용액
    two pointer
    
    우선 당연히 정렬을 하고,
    하나의 값을 기준으로 ( 그 인덱스를 idx 라고 하겠다.)
    그러면 idx보다 큰 인덱스에 있는 원소들을 가지고 0에 가까워질 수록 답을 갱신하면 된다.
    
    따라서 시간복잡도는 O(N^2)

---
+++ Code/2473.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+#define pb(x) push_back(x)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0)
+#define fileio() ifstream file_in("cowjump.in");ofstream file_out("cowjump.out")
+#define f first
+#define s second
+#define double long double
+#define db double
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tpi;
+typedef tuple<ll, ll, ll> tpl;
+typedef pair<double, ll> pdl;
+typedef pair<double, int> pdi;
+typedef pair<double, double> pdd;
+const int MOD = 987654321;
+const ll LMOD = 1e9+7; // PLZ check
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = 1e6+10;
+const int MAXR = 2e5 + 10;
+
+int main() {
+	fio();
+
+	int n;
+	cin >> n;
+	vector<ll> a(n);
+	for (int i = 0; i < n; i++) cin >> a[i];
+	sort(all(a));
+	ll md = LINF;
+	tpl ans;
+	for (int i = 0; i < n-2; i++) {
+		int le = i+1, ri = n-1;
+		while (le < ri) {
+			ll d = a[i]+a[le]+a[ri];
+			if (md > abs(d)) {
+				md = abs(d);
+				ans = {a[i], a[le], a[ri]};
+			}
+			if (d < 0) le++;
+			else if (d > 0) ri--;
+			else break;
+		}
+	}
+
+	auto [x,y,z] = ans;
+	cout << x << ' ' << y << ' ' << z << endl;
+	return 0;
+}
