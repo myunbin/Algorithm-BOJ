6c505bf4e1a972f9ab9a95636cdeaeca48f04468
Hyunbin Kim <akim9905@gmail.com>
2021-05-02 19:26:24 +0900

    패션왕 신해빈
    map

---
+++ Code/9375.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+#define pb(x) push_back(x)
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0)
+#define fileio() ifstream file_in("cowjump.in");ofstream file_out("cowjump.out")
+#define f first
+#define s second
+#define double long double
+#define db double
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tpi;
+typedef tuple<ll, ll, ll> tpl;
+typedef pair<double, ll> pdl;
+typedef pair<double, int> pdi;
+typedef pair<double, double> pdd;
+const int MOD = 1234567891;
+const ll LMOD = 1e9+7; // PLZ check
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = 111;
+const int MAXR = 2e5 + 10;
+
+void solve() {
+	int n; 
+	cin >> n;
+	map<string, int> m;
+	for (int i=0; i<n; i++) {
+		string a, b;
+		cin >> a >> b;
+		m[b]++;
+	}
+	ll ans = 1;
+	for (auto i=m.begin(); i != m.end(); i++) {
+		ans *= 1LL*(i->s+1);
+	}
+	cout << ans-1 << endl;
+}
+int main() {
+	fio();
+	int t;
+	cin >> t;
+	while (t--) solve();
+	return 0;
+}
