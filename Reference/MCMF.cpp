2598ccd6c3d4f701c8928a82e0d36b2d819721d7
Hyunbin Kim <akim9905@gmail.com>
2021-08-30 22:32:12 +0900

    Update MCMF.cpp

--- Reference/MCMF.cpp
+++ Reference/MCMF.cpp
@@ -1,49 +1,48 @@
 //https://justicehui.github.io/hard-algorithm/2020/03/24/effective-mcmf/
-
-const int SZ = 888;
+const int MAXN = 202020;
 struct MCMF{
-	int s, t; //source, sink
-	struct Edge{ int v, c, d, dual; };
-	vector<Edge> g[SZ];
-	void addEdge(int s, int e, int c, int d){
-		g[s].push_back({e, c, d, (int)g[e].size()});
-		g[e].push_back({s, 0, -d, (int)g[s].size()-1});
-	}
+    int s, t; //source, sink
+    struct Edge{ int v, c, d, dual; };
+    vector<Edge> g[MAXN];
+    void addEdge(int s, int e, int c, int d){
+        g[s].push_back({e, c, d, (int)g[e].size()});
+        g[e].push_back({s, 0, -d, (int)g[s].size()-1});
+    }
 
-	int dst[SZ], inq[SZ];
-	bool spfa(){
-		memset(dst, 0x3f, sizeof dst);
-		memset(inq, 0, sizeof inq);
-		queue<int> q; q.push(s); inq[s] = 1; dst[s] = 0;
-		while(q.size()){
-			int now = q.front(); q.pop(); inq[now] = 0;
-			for(auto i : g[now]){
-				if(i.c && dst[i.v] > dst[now] + i.d){
-					dst[i.v] = dst[now] + i.d;
-					if(!inq[i.v]) inq[i.v] = 1, q.push(i.v);
-				}
-			}
-		}
-		return dst[t] < 1e9;
-	}
+    int dst[MAXN], inq[MAXN];
+    bool spfa(){
+        memset(dst, 0x3f, sizeof dst);
+        memset(inq, 0, sizeof inq);
+        queue<int> q; q.push(s); inq[s] = 1; dst[s] = 0;
+        while(q.size()){
+            int now = q.front(); q.pop(); inq[now] = 0;
+            for(auto i : g[now]){
+                if(i.c && dst[i.v] > dst[now] + i.d){
+                    dst[i.v] = dst[now] + i.d;
+                    if(!inq[i.v]) inq[i.v] = 1, q.push(i.v);
+                }
+            }
+        }
+        return dst[t] < 1e9;
+    }
 
-	int chk[SZ], work[SZ];
-	bool update(){ //update shortest path DAG in O(V+E)
-		int mn = 1e9;
-        for(int i=0; i<SZ; i++){
+    int chk[MAXN], work[MAXN];
+    bool update(){ //update shortest path DAG in O(V+E)
+        int mn = 1e9;
+        for(int i=0; i<MAXN; i++){
             if(!chk[i]) continue;
             for(auto j : g[i]){
                 if(j.c && !chk[j.v]) mn = min(mn, dst[i] + j.d - dst[j.v]);
             }
         }
-		if(mn >= 1e9) return 0;
-		for(int i=0; i<SZ; i++){
-			if(!chk[i]) dst[i] += mn;
-		}
+        if(mn >= 1e9) return 0;
+        for(int i=0; i<MAXN; i++){
+            if(!chk[i]) dst[i] += mn;
+        }
         return 1;
-	}
+    }
 
-	int dfs(int now, int fl){
+    int dfs(int now, int fl){
         chk[now] = 1;
         if(now == t) return fl;
         for(; work[now] < g[now].size(); work[now]++){
@@ -57,21 +56,22 @@ struct MCMF{
             }
         }
         return 0;
-	}
-	p run(int _s, int _t){ //{cost, flow}
-		s = _s, t = _t;
-		int cst = 0, fl = 0;
-		spfa();
-		do{
-			memset(chk, 0, sizeof chk);
+    }
+    pii run(int _s, int _t){ //{cost, flow}
+        s = _s, t = _t;
+        int cst = 0, fl = 0;
+        spfa();
+        do{
+            memset(chk, 0, sizeof chk);
             memset(work, 0, sizeof work);
-			int now = 0;
-			while(now = dfs(s, 1e9)){
-				cst += dst[t] * now;
-				fl += now;
-				memset(chk, 0, sizeof chk);
-			}
-		}while(update());
-		return p(cst, fl);
-	}
+            int now = 0;
+            while(now = dfs(s, 1e9)){
+                cst += dst[t] * now;
+                fl += now;
+                memset(chk, 0, sizeof chk);
+            }
+        }while(update());
+        return pii(cst, fl);
+    }
 }
+
