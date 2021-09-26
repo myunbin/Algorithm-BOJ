bd60afc04d91f2444c444987ee83cd273fb1cd0f
Hyunbin Kim <akim9905@gmail.com>
2021-09-26 15:16:03 +0900

    Update fenwick tree.cpp

--- Reference/fenwick tree.cpp
+++ Reference/fenwick tree.cpp
@@ -7,13 +7,28 @@ struct fwt {
     void upd(int i, int v) {
         for (; i < tr.size(); i += (i&-i)) tr[i] += v;
     }
-    int qry(int r) { //[1,r]
-        int sum = 0;
-        for (; r; r -= (r&(-r))) sum += tr[r];
+    ll qry(int r) { //[1,r]
+        ll sum = 0;
+        for (; r; r -= (r&(-r))) sum += 1LL*tr[r];
         return sum;
     }
-    int qry(int l, int r) { //[l, r]
+    ll qry(int l, int r) { //[l, r]
         if(l == 0) return qry(r);
         return qry(r) - qry(l-1);
+    }
+
+    // find kth value
+    // let kth value be X, then kth(k) returns X+1 
+    int kth(int k){
+        int idx=0;
+        int lg = 32-__builtin_clz((int)tr.size())-1+1;
+        for(int i=lg;i>=0;i--){
+            if(idx+(1<<i) <= (1<<lg)-1 && tr[idx+(1<<i)] < k){
+                k-=tr[idx+(1<<i)];
+                idx=idx+(1<<i);
+            }
+        }
+        if(!k) return idx;
+        return idx+1;
     }
 };
